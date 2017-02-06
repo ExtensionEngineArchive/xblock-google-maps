3cc6b58c6dedbdcf4bfeeb199fb0817afef4899a
mferenca <marin.ferenca@gmail.com>
2017-02-06 13:34:33 +0100

    Implement the UI wireframe

--- google_maps/google_maps.py
+++ google_maps/google_maps.py
@@ -1,10 +1,14 @@
 """ Google Maps XBlock """
 
 import pkg_resources
+from django.utils.translation import ugettext_lazy as _
 
 from xblock.core import XBlock
-from xblock.fields import Scope, Integer
+from xblock.fields import Scope, Integer, String
 from xblock.fragment import Fragment
+from xblockutils.resources import ResourceLoader
+
+loader = ResourceLoader(__name__)
 
 
 class GoogleMapsXBlock(XBlock):
@@ -12,37 +16,78 @@ class GoogleMapsXBlock(XBlock):
     XBlock providing a Google Map to a course.
     """
 
+    display_name = String(
+        display_name=_('Display Name'),
+        default=_('Google Maps XBlock'),
+        help=_('The display name for this component.'),
+        scope=Scope.settings
+    )
+
+    api_key = String(
+        display_name=_('Google Maps API key'),
+        default='',
+        scope=Scope.content
+    )
+
+    place_name = String(
+        display_name=_('Search for a place'),
+        default='',
+        scope=Scope.settings
+    )
+
+    iframe_width = Integer(
+        display_name=_('Map width'),
+        default=600,
+        scope=Scope.settings
+    )
+
+    iframe_height = Integer(
+        display_name=_('Map height'),
+        default=450,
+        scope=Scope.settings
+    )
+
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
-        return data.decode("utf8")
-
+        return data.decode('utf8')
 
     def student_view(self, context=None):
         """
         The primary view of the GoogleMapsXBlock, shown to students
         when viewing courses.
         """
-        html = self.resource_string("public/html/google_maps.html")
-        frag = Fragment(html.format(self=self))
-        frag.add_css(self.resource_string("public/css/google_maps.css"))
-        frag.add_javascript(self.resource_string("public/js/src/google_maps.js"))
+
+        context = {
+            'self': self
+        }
+
+        html = self.resource_string('public/html/google_maps.html')
+        frag = Fragment()
+        frag.add_content(loader.render_template('/public/html/google_maps.html', context))
+        frag.add_css(self.resource_string('public/css/google_maps.css'))
+        frag.add_javascript(self.resource_string('public/js/src/google_maps.js'))
         frag.initialize_js('GoogleMapsXBlock')
         return frag
-
 
     def studio_view(self, context=None):
         """
         The primary view of the GoogleMapsXBlock, shown to staff members
         when editing courses.
         """
-        html = self.resource_string("public/html/google_maps_edit.html")
-        frag = Fragment(html.format(self=self))
-        frag.add_css(self.resource_string("public/css/google_maps_edit.css"))
-        frag.add_javascript(self.resource_string("public/js/src/google_maps_edit.js"))
+
+        context = {
+            'self': self,
+            'fields': self.fields
+        }
+
+        html = self.resource_string('public/html/google_maps_edit.html')
+        frag = Fragment()
+        frag.add_content(loader.render_template('/public/html/google_maps_edit.html', context))
+        frag.add_css(self.resource_string('public/css/google_maps_edit.css'))
+        frag.add_javascript(self.resource_string('public/js/src/google_maps_edit.js'))
         frag.initialize_js('GoogleMapsEditXBlock')
         return frag
-
 
     @XBlock.json_handler
     def studio_submit(self,data,suffix=''):
@@ -50,9 +95,11 @@ class GoogleMapsXBlock(XBlock):
         Called when submitting the form in studio.
         """
 
+        for key in data:
+            setattr(self, key, data[key])
+
         return { 'result':'success' }
 
- 
     @staticmethod
     def workbench_scenarios():
         """A canned scenario for display in the workbench."""
--- google_maps/public/js/src/google_maps_edit.js
+++ google_maps/public/js/src/google_maps_edit.js
@@ -1,16 +1,83 @@
 /* Javascript for GoogleMapsXBlock studio view */
 function GoogleMapsEditXBlock(runtime, element) {
-  /* Function for submiting input elements in edit mode */
-  $(element).on('click', '.save-button', function() {
+  var displayNameClearButton = $('.clear-display-name', element);
+  var displayNameDefaultValue = $('.edit-display-name', element).attr('data-default-value');
+  var displayNameInput = $('input[id=display-name-input]');
+  var iframeWidthClearButton = $('.clear-iframe-width', element);
+  var iframeWidthDefaultValue = $('.edit-iframe-width', element).attr('data-default-value');
+  var iframeWidthInput = $('input[id=iframe-width-input]');
+  var iframeHeightClearButton = $('.clear-iframe-height', element);
+  var iframeHeightDefaultValue = $('.edit-iframe-height', element).attr('data-default-value');
+  var iframeHeightInput = $('input[id=iframe-height-input]');
+
+  /**
+   * Sends input element values to the backend side
+   */
+  function saveXBlockData() {
     var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
 
-    $.post(handlerUrl, JSON.stringify({ })).done(function(response) {
+    var data = {
+      display_name: displayNameInput.val(),
+      place_name: $(element).find('input[id=place-name-input]').val(),
+      iframe_width: iframeWidthInput.val(),
+      iframe_height: iframeHeightInput.val(),
+      api_key: $(element).find('input[id=api-key-input]').val()
+    };
+
+    $.post(handlerUrl, JSON.stringify(data)).done(function(response) {
       window.location.reload(false);
     });
+  }
+
+  /**
+   * Closes studio editing modal window
+   */
+  function exitEditMode() {
+    runtime.notify('cancel', {});
+  }
+
+  /**
+   * Sets input element value to default and disables clear button
+   * @param {object} input element whose value needs to be set to default value
+   * @param {string} input element's default value
+   * @param {object} input element's clear button
+   */
+  function setDefaultValue(inputElement, defaultValue, clearButton) {
+    inputElement.val(defaultValue);
+    clearButton.prop('hidden', true);
+  }
+
+  $(document).ready(function() {
+    displayNameClearButton.prop('hidden', displayNameInput.val() === displayNameDefaultValue);
+    iframeWidthClearButton.prop('hidden', iframeWidthInput.val() === iframeWidthDefaultValue);
+    iframeHeightClearButton.prop('hidden', iframeHeightInput.val() === iframeHeightDefaultValue);
   });
 
-  /* Function for canceling */
-  $(element).on('click', '.cancel-button', function() {
-    runtime.notify('cancel', {});
+  $(element).on('keyup', 'input[id=display-name-input]', function() {
+    displayNameClearButton.prop('hidden', displayNameInput.val() === displayNameDefaultValue);
   });
+
+  $(element).on('click', '.clear-display-name', function() {
+    setDefaultValue(displayNameInput, displayNameDefaultValue, displayNameClearButton);
+  });
+
+  $(element).on('keyup mouseup', 'input[id=iframe-width-input]', function() {
+    iframeWidthClearButton.prop('hidden', iframeWidthInput.val() === iframeWidthDefaultValue);
+  });
+
+  $(element).on('click', '.clear-iframe-width', function() {
+    setDefaultValue(iframeWidthInput, iframeWidthDefaultValue, iframeWidthClearButton);
+  });
+
+  $(element).on('keyup mouseup', 'input[id=iframe-height-input]', function() {
+    iframeHeightClearButton.prop('hidden', iframeHeightInput.val() === iframeHeightDefaultValue);
+  });
+
+  $(element).on('click', '.clear-iframe-height', function() {
+    setDefaultValue(iframeHeightInput, iframeHeightDefaultValue, iframeHeightClearButton);
+  });
+  
+  $(element).on('click', '.save-button', saveXBlockData);
+
+  $(element).on('click', '.cancel-button', exitEditMode);
 }
